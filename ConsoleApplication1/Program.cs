cf4bc9c81f87977b1281a1ffa8bcc6994e5378c5
DESKTOP-FLFR2BI\Brad <scp_inc@hotmail.com>
2017-01-18 21:33:50 -0600

    Initial project

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 namespace ConsoleApplication1 {
     class Program {
         static void Main(string[] args) {
+            int StartPage = getStartPage();
+            string url = getURL();
+        }
+
+        private static string getURL() {
+            return "";
+        }
+
+        private static int getStartPage() {
+            return 0;
         }
     }
 }
