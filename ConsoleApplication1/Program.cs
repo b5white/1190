ee07bc92f4b9704fe64256cfd7a18d3c5ac17b63
DESKTOP-FLFR2BI\Brad <scp_inc@hotmail.com>
2017-01-18 22:33:06 -0600

    build url

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,23 +10,29 @@ namespace ConsoleApplication1 {
         static void Main(string[] args) {
             int StartPage = getStartPage();
             string url = getURL();
+            string folder = getURL();
             readPages(url, StartPage);
         }
 
-        private static void readPages(string url, int startPage) {
+        private static void readPages(string url, int startPage, string folder) {
             Boolean found = true;
-            while (found) {
-                found = readPage(buildURL(url, startPage));
+            string xml;
+            System.IO.StreamWriter stream = new System.IO.StreamWriter(folder + @"\" + startPage.ToString() + ".html"));
+            int stopPage = 25;
+            for (int i = startPage; i < stopPage; i++) {
+                found = readPage(buildURL(url, startPage), out xml);
+                writePage(stream, xml);
             }
         }
 
-        private static bool readPage(string v) {
+        private static bool readPage(string url, out string xml ) {
             throw new NotImplementedException();
         }
 
         private static string buildURL(string url, int startPage) {
-            return url + startPage.ToString();
+            return url + ((startPage - 1) * 40).ToString();
         }
+
         private static string getURL() {
             return "http://forums.xkcd.com/viewtopic.php?f=7&t=101043&start=";
         }
