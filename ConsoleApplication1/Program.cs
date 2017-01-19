7b3600de7a83a1ffb0cf8c473bb82088085b2cb4
DESKTOP-FLFR2BI\Brad <scp_inc@hotmail.com>
2017-01-19 00:16:51 -0600

    config file

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,24 +10,20 @@ namespace ConsoleApplication1 {
     class Program {
 
         static void Main(string[] args) {
-            int StartPage = getStartPage();
-            string url = getURL();
-            string folder = getFolder();
-            readPages(url, StartPage, folder);
+            Config config = new Config();
+            int startPage = getStartPage(config);
+            string url = getURL(config);
+            string folder = getFolder(config);
+            int stopPage = getStopPage(config);
+            readPages(url, startPage, stopPage, folder);
         }
 
-        private static string getFolder()
-        {
-            return @"C:\Users\Karen\Documents\xkcd\Time\Thread";
-        }
-
-        private static void readPages(string url, int startPage, string folder) {
+        private static void readPages(string url, int startPage, int stopPage, string folder) {
             Boolean found = true;
             string xml;
-            System.IO.StreamWriter stream = new System.IO.StreamWriter(folder + @"\" + startPage.ToString() + ".html");
-            int stopPage = 25;
+            System.IO.StreamWriter stream = new System.IO.StreamWriter(folder + @"\" + startPage.ToString() + ".html");           
             for (int i = startPage; i < stopPage; i++) {
-                found = readPage(buildURL(url, startPage), out xml);
+                found = readPage(buildURL(url, startPage), out xml);                
                 writePage(stream, xml);
             }
             stream.Close();
@@ -47,12 +43,23 @@ namespace ConsoleApplication1 {
             return url + ((startPage - 1) * 40).ToString();
         }
 
-        private static string getURL() {
-            return "http://forums.xkcd.com/viewtopic.php?f=7&t=101043&start=";
+        private static string getURL(Config config) {
+            //return "http://forums.xkcd.com/viewtopic.php?f=7&t=101043&start=";
+            return config.Value("BaseURL");
         }
 
-        private static int getStartPage() {
-            return 0;
+        private static int getStartPage(Config config) {
+            return int.Parse(config.Value("StartPage"));
         }
+        private static int getStopPage(Config config) {
+            //return 25;
+            return int.Parse(config.Value("StopPage"));
+        }
+
+        private static string getFolder(Config config) {
+            //return @"C:\Users\Karen\Documents\xkcd\Time\Thread";
+            return config.Value("Folder");
+        }
+
     }
 }
